9ccc45e12e921f56f1ebf93bfad1598dd9456e80
sabr2007 <sabyrzanvaliullov@gmail.com>
2026-03-17 22:57:53 +0500

    short fixes

--- main.py
+++ main.py
@@ -5,27 +5,22 @@ quantity = int(input("Enter quantity: "))
 
 subtotal = price * quantity
 discount = 0
-is_discount = False
 if subtotal > 5000:
     discount = (subtotal * 10) / 100
-    is_discount = True
 total = subtotal - discount
 
 print("==============================\n"
     "          SHOP RECEIPT\n"
     "==============================\n"
-    f"Customer: {cust_name}\n"
-    f"Product: {product_name}\n"
-    f"Price: {price} KZT\n"
-    f"Quantity: {quantity}\n"
+    f"Customer : {cust_name}\n"
+    f"Product : {product_name}\n"
+    f"Price : {price} KZT\n"
+    f"Quantity : {quantity}\n"
     "------------------------------\n"
-    f"Subtotal: {subtotal} KZT\n"
-    f"Discount: {discount} KZT\n"
-    f"Total: {total}\n"
+    f"Subtotal : {subtotal} KZT\n"
+    f"Discount : {discount} KZT\n"
+    f"Total : {total} KZT\n"
     "==============================" )
 
-print(f"Discount applied: {is_discount}")
-if total > 3000:
-    print("Paid more than 3000: True")
-else:
-    print("Paid more than 3000: False")
+print("Discount applied:", subtotal > 5000)
+print("Paid more than 3000:", total > 3000)
